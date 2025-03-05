d9a47b8efef1571ca811e8ae2c110036219946de
Tristen247 <139776061+Tristen247@users.noreply.github.com>
2025-03-04 19:01:46 -0500

    Add project files.

---
+++ BlockBusterWebApp/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+using System.Diagnostics;
+using BlockBusterWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlockBusterWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Colors() 
+        {
+            string[] colors = { "Red", "Blue", "Yellow" };
+            ViewBag.Colors = colors;
+            return View();
+        }
+
+        public IActionResult Cities()
+        {
+            string[] cities = { "Miami, FL", "Barcelona, Spain", "London, England", "Rome, Italy", "New York, NY" };
+            ViewBag.Cities = cities;
+            return View();
+        }
+
+        public IActionResult Hobbies()
+        {
+            string[] hobbies = { "Skiing and Snowboarding", "Mountain Biking", "Playing Video Games", "Watching Youtube/TV" };
+            ViewBag.Hobbies = hobbies; 
+            return View();
+        }
+
+    }
+}
